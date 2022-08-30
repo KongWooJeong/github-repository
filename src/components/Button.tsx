6ebe476f3469603d2510e4ae146a03c1478bd976
woojeong kong <coolstevevc@gmail.com>
2022-08-30 16:58:15 +0900

    refactor: Update button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,19 +2,39 @@ import React from "react";
 import styled from "styled-components";
 
 interface Props {
-  backgroundColor: string;
-  textColor: string;
+  backgroundColor?: string;
+  textColor?: string;
   text: string | number;
-  onClick: () => void;
-  isDisabled: boolean;
+  onClick?: () => void;
+  isDisabled?: boolean;
+  type?: "button" | "submit" | "reset";
 }
 
 interface StyleProps {
-  textColor: string;
-  backgroundColor: string;
+  textColor?: string;
+  backgroundColor?: string;
+}
+
+function Button({
+  backgroundColor,
+  textColor,
+  text,
+  onClick,
+  isDisabled,
+  type,
+}: Props) {
+  return (
+    <ButtonWrapper textColor={textColor} backgroundColor={backgroundColor}>
+      <button type={type} onClick={onClick} disabled={isDisabled}>
+        {text}
+      </button>
+    </ButtonWrapper>
+  );
 }
 
 const ButtonWrapper = styled.div<StyleProps>`
+  display: inline;
+
   button {
     display: inline-block;
     outline: 0;
@@ -25,8 +45,9 @@ const ButtonWrapper = styled.div<StyleProps>`
     line-height: 20px;
     border: 1px solid;
     border-radius: 6px;
-    color: ${({ textColor }) => textColor};
-    background-color: ${({ backgroundColor }) => backgroundColor};
+    color: ${({ textColor }) => (textColor ? textColor : "#0366d6")};
+    background-color: ${({ backgroundColor }) =>
+      backgroundColor ? backgroundColor : "#ffffff"};
     border-color: #1b1f2326;
     :hover {
       color: #ffffff;
@@ -35,19 +56,5 @@ const ButtonWrapper = styled.div<StyleProps>`
     }
   }
 `;
-
-function Button({
-  backgroundColor,
-  textColor,
-  text,
-  onClick,
-  isDisabled,
-}: Props) {
-  return (
-    <ButtonWrapper textColor={textColor} backgroundColor={backgroundColor}>
-      <button onClick={onClick}>{text}</button>
-    </ButtonWrapper>
-  );
-}
 
 export default Button;
