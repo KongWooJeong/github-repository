146d157560be91b1ce4224e0b67074dfca2c5925
woojeong kong <coolstevevc@gmail.com>
2022-08-30 16:58:41 +0900

    refactor: Update textInput component styles

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -4,18 +4,16 @@ const TextInput = styled.input`
   background-color: #f7fafc;
   color: lightgray;
   height: 52px;
-  padding: 0 0 8px;
   font-weight: 500;
   font-size: 30px;
   border-radius: 1px;
-  caret-color: skyblue;
   outline: none;
   border: 0 none;
   border-bottom: 2px solid lightgray;
+
   :focus {
     border-bottom-color: skyblue;
   }
-  transition: background-color 0.2s ease;
 `;
 
 export default TextInput;
